29acb5797b5f512d60f5ac88c42443df421c94a7
mtpl-dev-5 <morphsync.dev.005@gmail.com>
2026-02-27 14:31:26 +0530

    .

--- src/screens/admin/AdminCustomerManagement.js
+++ src/screens/admin/AdminCustomerManagement.js
@@ -477,7 +477,7 @@ const AdminCustomerManagement = ({ showToast }) => {
                 statusBarTranslucent={true}
             >
                 <KeyboardAvoidingView
-                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    behavior={Platform.OS === 'ios' ? 'padding' : undefined}
                     style={{ flex: 1 }}
                 >
                     <View style={styles.modalOverlay}>
